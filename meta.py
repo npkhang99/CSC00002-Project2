8d7b6e76b5590ed30d2ac59b8ea6f5894ca1572c
Nguyễn Phúc Khang <mansblacks@users.noreply.github.com>
2018-05-17 18:01:14 +0700

    Update
    Update file tạo meta files giống với yêu cầu của thầy

--- meta.py
+++ meta.py
@@ -10,38 +10,52 @@ import config as cfg
 def create_meta_file(path):
     if path[-1] != '/':
         path += '/'
-    pics = glob.glob(path + '*.jpg')
     # print(path)
-    n = len(pics)
-    random.shuffle(pics)
-    meta = {
-        "TRAINING": [],
-        "VALIDATING": [],
-        "TESTING": []
-    }
-    with open(path + 'meta.json', 'w') as f:
-        for i in range(int(n * cfg.TRAIN_RATIO)):
-            # f.write(pics[0] + '\n')
-            meta["TRAINING"].append(pics[0])
-            pics.pop(0)
-        for i in range(int(n * cfg.VALIDATE_RATIO)):
-            # f.write(pics[0] + '\n')
-            meta["VALIDATING"].append(pics[0])
-            pics.pop(0)
+    validate = {}
+    test = {}
+    train = {}
+    uid = {}
+    cnt = 0
+    for folder in sorted(glob.glob(path + '*/')):
+        print('Processing {:>30s}- uid: {}'.format(folder, cnt))
+        name = folder.split('/')[-2]
+        uid[name] = cnt
+        cnt += 1
+        files = glob.glob(folder + '*')
+        random.shuffle(files)
+        n = len(files)
+        validate[name] = []
+        test[name] = []
+        train[name] = []
         for i in range(int(n * cfg.TEST_RATIO)):
-            # f.write(pics[0] + '\n')
-            meta["TESTING"].append(pics[0])
-            pics.pop(0)
-        # print(meta)
-        f.write(json.dumps(meta, indent = 4))
+            test[name].append(files[0])
+            files.pop(0)
+        for i in range(int(n * cfg.VALIDATE_RATIO)):
+            validate[name].append(files[0])
+            files.pop(0)
+        for i in range(int(n * cfg.TRAIN_RATIO)):
+            train[name].append(files[0])
+            files.pop(0)
+    
+    # print meta files
+    with open('validate.json', 'w') as f:
+        f.write(json.dumps(validate, indent = 4))
         f.close()
-    print('Meta file created for {}'.format(path))
+    with open('test.json', 'w') as f:
+        f.write(json.dumps(test, indent = 4))
+        f.close()
+    with open('train.json', 'w') as f:
+        f.write(json.dumps(train, indent = 4))
+        f.close()
+    with open('uid.json', 'w') as f:
+        f.write(json.dumps(uid, indent = 4))
+        f.close()
+    print('Meta files created')
 
 def main(path):
     if path[-1] != '/':
         path += '/'
-    for folder in glob.glob(path + '*/'):
-        create_meta_file(folder)
+    create_meta_file(path)
 
 if __name__ == '__main__':
     main(sys.argv[1])
